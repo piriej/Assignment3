20aaa2783dab6c4ca9dcc58cbcd90dcee74155f8
Jim <jtulip@csu.edu.au>
2015-08-25 03:03:06 +1000

    got basic main menu - borrow nav working

---
+++ NewLibrary/NewLibrary/Library/MainController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+using NewLibrary;
+
+
+namespace NewLibrary.Library
+{
+
+    public class MainController
+    {
+        private MainWindow mainWindow;
+
+
+        public MainController(MainWindow mainWindow)
+        {
+            this.mainWindow = mainWindow;
+        }
+
+
+        public void Initialise()
+        {
+            mainWindow.MainPanel.Visibility = Visibility.Visible;
+        }
+
+        public void borrowSelected()
+        {
+            mainWindow.MainPanel.Visibility = Visibility.Hidden;
+            mainWindow.BorrowPanel.Visibility = Visibility.Visible;
+        }
+
+
+        public void mainSelected()
+        {
+            mainWindow.BorrowPanel.Visibility = Visibility.Hidden;
+            mainWindow.MainPanel.Visibility = Visibility.Visible;
+        }
+    }
+
+}
---
+++ NewLibrary/NewLibrary/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using NewLibrary.Library;
+using NewLibrary.Library.Panels.Borrow;
+
+namespace NewLibrary
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainController controller;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            controller = new MainController(this);
+            controller.Initialise();
+            MainPanel.setController(controller);
+            BorrowPanel.setController(controller);
+        }
+
+    }
+}
