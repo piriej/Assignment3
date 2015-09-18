5f2141bbed9391c33cdb8d8cb857ae52dbf2269c
Jeff <jpirie@iinet.net.au>
2015-09-18 10:53:05 +1000

    Refactored test to use page object pattern

---
+++ AcceptanceTests/Steps/Arrange/AutomationElementHelpers.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading;
+using System.Windows.Automation;
+
+namespace AcceptanceTests.Steps.Arrange
+{
+    static class AutomationElementHelpers
+    {
+        const int ImplicitWait = 2;
+        const int ImplicitWaitInterval = 1000;
+
+        public static AutomationElement FindByName(this AutomationElement root, string name, int wait = ImplicitWait)
+        {
+            var waitCount = 0;
+            AutomationElement node;
+            do
+            {
+                Thread.Sleep(ImplicitWaitInterval);
+                node = root.FindFirst(
+                    TreeScope.Descendants,
+                    new PropertyCondition(AutomationElement.NameProperty, name));
+                waitCount++;
+            } while (node == null || waitCount == wait);
+
+            return node;
+        }
+
+        public static AutomationElement FindById(this AutomationElement root, string id, int wait = ImplicitWait)
+        {
+
+            var waitCount = 0;
+            AutomationElement node;
+            do
+            {
+                Thread.Sleep(ImplicitWaitInterval);
+                node = root.FindFirst(
+                    TreeScope.Descendants,
+                    new PropertyCondition(AutomationElement.AutomationIdProperty, id));
+                waitCount++;
+            } while (node == null || waitCount == wait);
+
+            return node;
+        }
+
+    }
+}
--- AcceptanceTests/Steps/Arrange/SwipeWindowArrangeSteps.cs
+++ AcceptanceTests/Steps/Arrange/SwipeWindowArrangeSteps.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
-using System.Windows.Automation;
+using AcceptanceTests.PageObjects;
 using FluentAssertions;
 using TechTalk.SpecFlow;
 
@@ -16,70 +15,22 @@ namespace AcceptanceTests.Steps.Arrange
         public void GivenTheLoanSelfServiceStationPromptsTheUserToSwipeTheirCard()
         {
             // Identify the window components.
+            //Thread.Sleep(2000);
+            //var app = AutomationElement.RootElement.FindByName("MainWindow");
+            //var automationElement = app.FindById("borrowButton");
 
-            //List<AutomationElement> messages = new List<AutomationElement>();
-            //TreeWalker walker = new TreeWalker(new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.DataItem));
+            //automationElement.Should().NotBeNull();
 
-            //AutomationElement row = walker.GetFirstChild(AutomationElement.RootElement);
-            //while (row != null)
-            //{
-            //    //if (/*some condition*/)
-            //    messages.Add(row);
-            //    row = walker.GetNextSibling(row);
-            //}
+            //automationElement.GetCurrentPropertyValue(AutomationElement.IsEnabledProperty).Should().Be(true);
 
-            //// MainWindow - MainMenuControl
-            //var automationElement = AutomationElement.RootElement.FindFirst(TreeScope.Children,
-            //   new PropertyCondition(AutomationElement.NameProperty, "MainMenuControl"));
-            Thread.Sleep(2000);
-            var app = AutomationElement.RootElement.FindByName("MainWindow");
-            var automationElement = app.FindById("borrowButton");
-           // var test = AutomationElement.RootElement.FindAll(TreeScope.Descendants, )
-            AutomationElementCollection collection = app.FindAll(TreeScope.Descendants, Condition.TrueCondition);
-            Console.WriteLine("****************************************************************************");
-            Console.WriteLine("Any?" + collection.Count);
-            foreach (AutomationElement ae in collection)
-            {
-                Console.WriteLine("Here:" + ae.Current.Name + "," + ae.Current.AutomationId + "," + ae.Current.ClassName);
-            }
-            //var automationElement2 = AutomationElement.RootElement.Find("label");
-            Console.WriteLine("****************************************************************************");
-            automationElement.Should().NotBeNull();
+            //var patterns = automationElement.GetSupportedPatterns();
+            //var invokePattern = automationElement.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+            //invokePattern.Invoke();
+            var pageObject = new MainWindowPageObject();
 
-            automationElement.GetCurrentPropertyValue(AutomationElement.IsEnabledProperty).Should().Be(true);
+            pageObject.IsBorrowButtonEnabled().Should().BeTrue("The borrow button is not enabled.");
 
-            var patterns = automationElement.GetSupportedPatterns();
-            var invokePattern = automationElement.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
-            invokePattern.Invoke();
-
-            ////AutomationElement child = walker.GetFirstChild(el);
-            //var point = automationElement.GetClickablePoint();
-            //Mouse.Move((int)point.X, (int)point.Y);
-            //Mouse.Click(MouseButton.Left);
-
-            //Condition windowCondition = new PropertyCondition(AutomationElement.NameProperty, "Window Title");
-
-            // AutomationElement window = AutomationElement.RootElement.FindFirst(TreeScope.Element, windowCondition);
-
-
-        }
-
-    }
-
-    static class AutomationElementHelpers
-    {
-        public static AutomationElement FindByName(this AutomationElement root, string name)
-        {
-            return root.FindFirst(
-             TreeScope.Descendants,
-             new PropertyCondition(AutomationElement.NameProperty, name));
-        }
-
-        public static AutomationElement FindById(this AutomationElement root, string name)
-        {
-            return root.FindFirst(
-             TreeScope.Descendants,
-             new PropertyCondition(AutomationElement.AutomationIdProperty, name));
+            pageObject.ClickBorrowButton();
         }
 
     }
