6cbbf2c468a88cdfd2e00932f863c4772d39e687
Jeff <jpirie@iinet.net.au>
2015-09-28 23:58:34 +1000

    Added scanner control

--- Features/MainWindow/MainWindowViewModel.cs
+++ Features/MainWindow/MainWindowViewModel.cs
@@ -2,6 +2,7 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using Library.Features.Scanner;
 using Library.Interfaces.Hardware;
 using Prism.Commands;
 using Prism.Events;
@@ -21,7 +22,8 @@ namespace Library.Features.MainWindow
 
         public ICardReader CardReader { get; set; }
         //public IScanner Scanner { get; set; }
-        public IPrinter Printer { get; set; }
+        public IScannerViewModel ScannerViewModel {get; set;}
+        //public IPrinter Printer { get; set; }
 
         #endregion
 
@@ -29,7 +31,7 @@ namespace Library.Features.MainWindow
         public MainWindowViewModel(IEventAggregator eventAggregator)
         {
             _eventAggregator = eventAggregator;
-            _eventAggregator.GetEvent<Messages.CloseApplicationEvent>().Subscribe( x => CloseWindow());
+            _eventAggregator.GetEvent<Messages.CloseApplicationEvent>().Subscribe(x => CloseWindow());
             CloseWindowCommand = new DelegateCommand(CloseWindow);
         }
 
